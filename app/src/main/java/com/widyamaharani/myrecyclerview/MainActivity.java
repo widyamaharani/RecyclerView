a59c3dbaeb8c5f1b1fac319391dd186b32509302
widyamaharani <madewidya237@gmail.com>
2021-03-30 23:35:49 +0800

    RecyclerView

---
+++ app/src/main/java/com/widyamaharani/myrecyclerview/MainActivity.java
@@ -0,0 +1,32 @@
+package com.widyamaharani.myrecyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    String s1[], s2[];
+    int images[] = {R.drawable.begonia, R.drawable.dahlia, R.drawable.lantana, R.drawable.lilac,
+            R.drawable.lobelia, R.drawable.fuchsia, R.drawable.petunia, R.drawable.matahari,
+            R.drawable.sweetalyssum, R.drawable.teratai};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        s1 = getResources().getStringArray(R.array.bunga);
+        s2 = getResources().getStringArray(R.array.description);
+
+        MyAdapter myAdapter = new MyAdapter(this, s1, s2, images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
